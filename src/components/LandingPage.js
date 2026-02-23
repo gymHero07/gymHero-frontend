6b71f7de594f79af2f845cfecd21ebafc67b1432
Kantemir <kan.shandir94@gmail.com>
2026-02-23 12:33:32 +0300

    created landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <header className="landing-header">
+        <h1 className="landing-title">GymHero</h1>
+        <p className="landing-subtitle">Press start to begin your training</p>
+        <div className="landing-actions">
+          <button className="landing-button landing-button-primary">
+            Start Training
+          </button>
+          <button className="landing-button landing-button-secondary">
+            Settings
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
+
